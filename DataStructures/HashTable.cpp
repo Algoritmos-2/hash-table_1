c06bdebd234e0673cba6638d5c5dbc2e77a6cabb
Joaquin Vigna <joaquin@brokenrubik.co>
2022-03-20 19:28:13 -0300

    empty hash table

---
+++ DataStructures/HashTable.cpp
@@ -0,0 +1,41 @@
+#ifndef TABLA_HASH
+#define TABLA_HASH
+#include <cassert>
+#include <string>
+#include <iostream>
+using namespace std;
+
+// TODO:
+// - Implementar la Tabla de hash
+
+class HashTable
+{
+private:
+public:
+    HashTable(int size)
+    {
+        // TODO
+    }
+    ~HashTable()
+    {
+        // TODO
+    }
+    void insertar(string clave, int valor)
+    {
+        // TODO
+    }
+    void eliminar(string clave)
+    {
+        // TODO
+    }
+    int recuperar(string clave)
+    {
+        // TODO
+    }
+    bool existe(string clave)
+    {
+        // TODO
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+#include "./DataStructures/HashTable.cpp"
+using namespace std;
+
+int main()
+{
+    HashTable * tabla = new HashTable(10);
+    return 0;
+}
