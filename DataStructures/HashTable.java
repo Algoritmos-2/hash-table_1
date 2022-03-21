cfc2522bedf4ac64dca9cc4ea0380a894d214404
Joaquin Vigna <joaquin@brokenrubik.co>
2022-03-20 21:59:14 -0300

    adding java initial files

---
+++ DataStructures/HashTable.java
@@ -0,0 +1,28 @@
+package data_structures;
+
+// TODO:
+// - Implementar la Tabla de hash
+
+public class HashTable {
+
+    public HashTable() {
+        // TODO
+    }
+
+    public void insertar(string clave, int valor)
+    {
+        // TODO
+    }
+    public void eliminar(string clave)
+    {
+        // TODO
+    }
+    public int recuperar(string clave)
+    {
+        // TODO
+    }
+    public boolean existe(string clave)
+    {
+        // TODO
+    }
+}
