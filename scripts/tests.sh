1f5535ea7a698763b547200f02f646de037a660c
Joaquin Vigna <joaquin@brokenrubik.co>
2022-03-28 16:36:39 -0300

    modify test template

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -9,31 +9,28 @@ TEST_FOLDER=./Tests
 JAVAFILE=$FOLDER/Main.java
 CPPFILE=$FOLDER/main.cpp
 
-
 # borrando resultados anteriores
 find $TEST_FOLDER -name "*.own.txt" -type f -delete
-
 # echo "Empezando las pruebas"
 du $TEST_FOLDER/*.in.txt | sort -g |
 while read filesize filename; do
     Ts="$(date +%s)"
     Tmm="$(date +%s%3N)"
     if [ -f "$CPPFILE" ]; then
-    $BUILD_FOLDER/main.out < $filename > ${filename/in/own}
+        $BUILD_FOLDER/main.out < $filename > ${filename/in/own}
     else 
-    java Main < $filename > ${filename/in/own}
+        java Main < $filename > ${filename/in/own}
     fi
     Ts="$(($(date +%s)-Ts))"
     Tmm="$(($(date +%s%3N)-Tmm))"
     echo "$filename : $Ts segundos | $Tmm milisegundos"
     diff -Z -B ${filename/in/out} ${filename/in/own} > /dev/null
-    if [ $? -eq 0 ]
-    then
-    echo -e "\e[32m${filename} - OK\e[0m"
+
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32m${filename} - OK\e[0m"
+        continue
     else
-    echo -e "\e[31m${filename} - FAIL\e[0m"
-    exit 1 # terminate and indicate error
+        echo -e "\e[31m${filename} - FAIL\e[0m"
+        exit 1
     fi
-done
-# borra todo compilado de java
-find . -name "*.class" -type f -delete
+done || exit 1
